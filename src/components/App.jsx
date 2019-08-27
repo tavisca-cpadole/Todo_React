79ab5c76c160f9cbce001105c5149c731ca78845
Padole <cpadole@internal.tavisca.com>
2019-08-27 12:29:33 +0530

    Seperated Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,65 @@
 import React, { Component } from "react";
-import Content from "./Content";
-import NavBar from "./NavBar";
+import Content from "./Content.jsx";
+import NavBar from "./NavBar.jsx";
 
 class App extends Component {
-  state = {};
+  state = {
+    sideList: [
+      { name: "Chinmay Padole", selected: false },
+      { name: "Rameez Parkar", selected: false },
+      { name: "Saurabh Mishra", selected: false }
+    ]
+  };
+
+  onUpdateClick(data) {
+    const updatedList = this.state.sideList.map((user, index) => {
+      if (index === data.id)
+        return { name: data.fName + " " + data.lName, selected: true };
+      else return { name: user.name, selected: false };
+    });
+
+    //console.log(updatedList);
+    this.setState({
+      sideList: updatedList,
+      fName: data.fName,
+      lName: data.lName,
+      id: data.id
+    });
+  }
+  async onNavBarClick(data) {
+    let name = data.name.split(" ");
+
+    const updatedList = this.state.sideList.map(user => {
+      if (user.name === data.name) return { name: user.name, selected: true };
+      else return { name: user.name, selected: false };
+    });
+
+    await this.setState({
+      sideList: updatedList,
+      fName: name[0],
+      lName: name[1],
+      id: data.id
+    });
+  }
+
   render() {
     return (
       <div id="main">
-        <Content />
-        <NavBar />
+        {this.state.fName && (
+          <Content
+            fName={this.state.fName}
+            lName={this.state.lName}
+            id={this.state.id}
+            onUpdateClick={this.onUpdateClick.bind(this)}
+          />
+        )}
+        <NavBar
+          sideList={this.state.sideList}
+          onNavBarClick={this.onNavBarClick.bind(this)}
+          fName={this.state.fName}
+          lName={this.state.lName}
+          id={this.state.id}
+        />
       </div>
     );
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+
 class NavBar extends Component {
-  state = {};
-  render() {
-    return <h1>Hello WOrld</h1>;
+  state = {
+    sideList: this.props.sideList,
+    fName: this.props.fName,
+    lName: this.props.lName,
+    id: this.props.id
+  };
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      sideList: nextProps.sideList,
+      fName: nextProps.fName,
+      lName: nextProps.lName,
+      id: nextProps.id
+    });
+    //console.log("componentWillReceiveProps", nextProps.fName);
   }
 
   renderList() {
@@ -14,12 +27,49 @@ class NavBar extends Component {
           <li
             className={tag.selected === true ? "text-success" : ""}
             key={index}
-            onClick={() => this.onItemClick(index)}
+            onClick={() =>
+              this.props.onNavBarClick({
+                name: tag.name,
+                id: index
+              })
+            }
           >
             {tag.name}
           </li>
         ))}
       </ul>
+    );
+  }
+
+  //   onItemClick = i => {
+  //     let name = this.state.sideList[i].name.split(" ");
+
+  //     const updatedList = this.state.sideList.map(user => {
+  //       if (user.name === name[0] + " " + name[1])
+  //         return { name: user.name, selected: true };
+  //       else return { name: user.name, selected: false };
+  //     });
+  //     // this.setState({
+  //     //   sideList: updatedList,
+  //     //   fName: name[0],
+  //     //   lName: name[1],
+  //     //   id: i
+  //     // });
+  //     const data = {
+  //       sideList: updatedList,
+  //       fName: name[0],
+  //       lName: name[1],
+  //       id: i
+  //     };
+  //     this.props.onNavBarClick(data);
+  //   };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        {this.renderList()}
+      </div>
     );
   }
 }
